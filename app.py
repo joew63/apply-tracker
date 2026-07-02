19d3dd5f27dc1453859169e55dcdaf919ce7cc8e
joewah <joeyu88775@gmail.com>
2026-07-02 12:19:14 -0700

    fixed bugs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from database import app_create, add_company, add_application
+from database import app_create, add_company, add_application, get_all_applications
 
 app = Flask(__name__)
 app_create()
@@ -9,18 +9,18 @@ def main():
     if request.method == 'POST':
         form_type = request.form['form_type']
         if form_type == 'add_company':
-            add_company(request.form.get('name'))
+            add_company(request.form.get('name').capitalize())
         elif form_type == 'add_application':
             add_application(
-                request.form.get('name'),
-                request.form.get('role'),
-                request.form.get('status'),
+                request.form.get('name').capitalize(),
+                request.form.get('role').title(),
+                request.form.get('status').capitalize(),
                 request.form.get('date_applied'),
-                request.form.get('notes')
+                request.form.get('notes').capitalize()
             )
         return redirect(url_for('main'))
     print('hello')
-    return render_template('index.html')
-
+    applications = get_all_applications()
+    return render_template('index.html', applications=applications)
 if __name__ == "__main__":
     app.run(debug=True)
